774b7aabcb2afa7a52762152c474229d6b5ac395
Julia <yuliyafedyakova1986@mail.ru>
2018-03-27 16:33:37 +0300

    коммент

--- lesson1/lesson1-hard/script-hard.js
+++ lesson1/lesson1-hard/script-hard.js
@@ -1,15 +1,15 @@
-var num = 33721;
+var num = '33721';
 var res = 1;
 var i = 0;
 var length = String(num).length;
-do {
-	res *= num % 10;
-	num = Math.floor(num/10);
-		console.log(res);
-	i++;
-}
-while (i < length);
-	alert(Math.pow(res, 3));
+// do {
+// 	res *= num % 10;
+// 	num = Math.floor(num/10);
+// 		console.log(res);
+// 	i++;
+// }
+// while (i < length);
+// 	alert(Math.pow(res, 3));
 
 // var digit5 = num % 10;
 // var digit4 = Math.floor(num % 100 / 10);
@@ -17,4 +17,12 @@ while (i < length);
 // var digit2 = Math.floor(num % 10000 / 1000);
 // var digit1 = Math.floor(num % 100000 / 10000);
 // var sum = digit5 * digit4 * digit3 * digit2 * digit1;
+
 // var sum = eval(eval("'33721'.split('').join('*');"));
+
+
+for(i = 0; i < num.length; i++) {
+	res *= +num.charAt(i);
+	console.log(res);
+}
+alert(Math.pow(res, 3));
