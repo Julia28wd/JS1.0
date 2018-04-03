84acbfc68ed743b2b1efb6617f056ab9cd7e465f
Julia <yuliyafedyakova1986@mail.ru>
2018-04-03 14:03:47 +0300

    comment

---
+++ lesson7/js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function() { //пока не загрузятся все элемнты DOM
+
+	let tab = document.getElementsByClassName('info-header-tab'), //заголовки пунктов подменю
+		tabContent = document.getElementsByClassName('info-tabcontent'), //обертка для дивов с содержанием
+		info = document.getElementsByClassName('info-header')[0]; //обертка для заголовков подменю
+
+	function hideTabContent(a) { //функция для скрытия ненужных дивов с контентом
+		for(let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show'); //удаляем класс show
+			tabContent[i].classList.add('hide'); //добавляем класс hide
+		}
+
+	}
+	hideTabContent(1); //по умолчанию первый контент показывать, поэтому его не трогать
+
+	function showTabContent(b) { //функция для показа нужных дивов с контентом
+		if(tabContent[b].classList.contains('hide')) { //если див скрыт ...
+			hideTabContent(0); //скрываем первый
+			tabContent[b].classList.remove('hide'); //удаляем класс hide
+			tabContent[b].classList.add('show'); //добавляем класс show
+		}
+
+	}
+
+	info.addEventListener('click', function(event) {
+		let target = event.target;
+		if(target.className == 'info-header-tab') {
+			for(let i = 0; i < tab.length; i++) { 
+				if(target == tab[i]) { //при клике на i-й таб применяем функцию showTabContent
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+});
